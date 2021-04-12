bba890b4787cd034f8b3997a23c3bdc308572c80
Cr4zi <61344888+Cr4zi@users.noreply.github.com>
2021-04-12 15:12:44 +0300

    Creating deafult commands for the game.

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -5,6 +5,26 @@ class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def create_game(self, ctx):
+        pass
+
+    @commands.command()
+    async def join_game(self, ctx):
+        pass
+
+    @commands.command()
+    async def kill(self, ctx, target: discord.Member):
+        pass
+
+    @commands.command()
+    async def heal(self, ctx, target: discord.Member):
+        pass
+    
+    @commands.command()
+    async def ask(self, ctx, target: discord.Member):
+        pass
 
 def setup(bot):
     bot.add_cog(Commands(bot))
